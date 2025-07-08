53614aa78ee4916b1b935fa4b70ecd29317b4f5b
Jimmy McCue <xstreamfishn@gmail.com>
2025-07-07 22:46:32 -0400

    Added events for ship moving left and right within boudaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,7 @@ class AlienInvasion:
        #Game loop
        while self.running:
           self._check_events()
-                
+          self.ship.update()  
           self._update_screen()
           self.clock.tick(self.settings.FPS)
 
@@ -37,10 +37,31 @@ class AlienInvasion:
 
     def _check_events(self):
         for event in pygame.event.get():
-           if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:
               self.running = False
               pygame.quit()
               sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keyup_events(self, event):
+       if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+       elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _check_keydown_events(self, event):
+       if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+       elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+       elif event.key == pygame.K_q:
+            self.running = False
+            pygame.quit()
+            sys.exit()
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
