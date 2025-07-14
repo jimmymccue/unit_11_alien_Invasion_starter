903e0920d1ef39a5f9eb270e82dcb0812d87f5cd
Jimmy McCue <xstreamfishn@gmail.com>
2025-07-14 18:54:52 -0400

    P2- Set up alien fleet for sound and collisions with projectiles and bottom

--- alien_invasion.py
+++ alien_invasion.py
@@ -50,7 +50,14 @@ class AlienInvasion:
         # check collisions for ship
         if self.ship.check_collisions(self.alien_fleet.fleet):
             self._reset_level()
-
+        # check collisions for aliens and bottom of screen
+        if self.alien_fleet.check_fleet_bottom():
+            self._reset_level()
+        # check collisions for projectiles and aliens
+        collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
+        if collisions:
+            self.impact_sound.play()
+            self.impact_sound.fadeout(500)
 
     def _reset_level(self):
         self.ship.arsenal.arsenal.empty()
